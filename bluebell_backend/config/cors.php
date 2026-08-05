3f8aa5759ad7befb62bbfd79f7742b6c098ef6fc
Sabitha <sabithamohandast@gmail.com>
2026-08-05 16:26:03 +0400

    Add Vercel frontend URL to CORS allowed origins

--- bluebell_backend/config/cors.php
+++ bluebell_backend/config/cors.php
@@ -20,7 +20,8 @@ return [
     'allowed_methods' => ['*'],
 
     // 'allowed_origins' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'],
+    // 'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['http://localhost:5173', 'https://bluebell-bakes.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
