a5dff598122e93e26569f221e8675140e81e287f
Sabitha <sabithamohandast@gmail.com>
2026-08-11 12:47:43 +0400

    Spell case Correction

--- bluebell_backend/routes/api.php
+++ bluebell_backend/routes/api.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\CategoryController;
-use App\Http\Controllers\Api\DashboardController;
+use App\Http\Controllers\API\DashboardController;
 use App\Http\Controllers\API\OrderController;
 use App\Http\Controllers\CartItemController;
 use App\Http\Controllers\ProductController;
